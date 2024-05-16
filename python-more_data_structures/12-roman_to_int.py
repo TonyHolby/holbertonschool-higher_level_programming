d4e92f12b507d8568ff2cc609f6f7bb3ade521d9
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-16 20:37:49 +0200

    Fix code in 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_value = 0
-    if not roman_string or roman_string is None:
-        return 0
     for letter in range(len(roman_string)):
         if letter < len(roman_string) - 1:
             if roman_d[roman_string[letter]] < roman_d[roman_string[letter+1]]:
