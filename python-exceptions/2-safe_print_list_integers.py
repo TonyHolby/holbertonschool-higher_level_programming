2a0b3a55654dbacecd6a1fdd0f91c68e3111c525
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-17 14:42:01 +0200

    Fix code in 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed_number = 0
-    for index in range(x):
+    for index in range(0, x):
         try:
             print("{:d}".format(my_list[index]), end="")
         except (TypeError, ValueError):
