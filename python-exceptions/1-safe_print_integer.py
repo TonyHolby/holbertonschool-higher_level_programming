9c644f9dc34e4977a6a6989101d2d503199d58cb
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-17 09:53:42 +0200

    Fix code in 1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int) is False:
-            return False
+        if isinstance(value, int) is True:
+            print("{}".format(value))
+            return True
         else:
             raise ValueError
     except ValueError:
-            print("{}".format(value))
-    return True
+            return False
