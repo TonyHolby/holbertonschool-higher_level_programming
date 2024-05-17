27b914c808d9d5216c4ef3231d538a9aa71f4381
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-17 14:26:53 +0200

    Change the code in 1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value == "" or value is None:
-        print()
-        return True
     try:
-        if isinstance(value, int):
-            print("{}".format(value))
-            return True
-        elif isinstance(value, str):
-            for character in value:
-                if character.isdigit() is True:
-                    continue
-                else:
-                    return False
-            print("{}".format(value))
-            return True
-        else:
-            raise TypeError
-    except TypeError:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
         return False
