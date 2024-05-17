e3f38d9523197b2a1bdb13ab56452c092bf781d8
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-17 14:37:32 +0200

    Fix code in 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,8 @@ def safe_print_list_integers(my_list=[], x=0):
     printed_number = 0
     for index in range(x):
         try:
-            if isinstance(my_list[index], int):
-                print("{}".format(my_list[index]), end="")
-                printed_number += 1
-            else:
-                raise ValueError
-        except ValueError:
+            print("{:d}".format(my_list[index]), end="")
+        except (TypeError, ValueError):
             continue
     print()
     return printed_number
