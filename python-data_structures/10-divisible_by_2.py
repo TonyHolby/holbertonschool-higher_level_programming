c53f442e27459cf15be91b69f654bdba8308e377
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-18 15:57:51 +0000

    Fix code in 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     pair_list = []
-    for number in my_list:
-        if my_list[number] % 2 == 0:
+    for index, element in enumerate(my_list):
+        if element % 2 == 0:
             pair_list.append(True)
         else:
             pair_list.append(False)
