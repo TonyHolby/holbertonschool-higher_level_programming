206ddfa5b8ff2744118951954ab0b4f2eb7354bb
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-18 15:12:28 +0000

    Fix code in 7-add_tupple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,13 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a
     b = tuple_b
-    a = a + (0, 0) if len(a) < 2 else a[:2]
-    b = b + (0, 0) if len(b) < 2 else b[:2]
+    if len(a) < 2:
+        a = a + (0, 0)
+    else:
+        a[:2]
+    if len(b) < 2:
+        b = b + (0, 0)
+    else:
+        b[:2]
     result = (a[0] + b[0], a[1] + b[1])
     return result
