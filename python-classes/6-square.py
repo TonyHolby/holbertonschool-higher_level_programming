83344b4dd19ec33dd306bf7a1a0a11ba327ae2db
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-18 23:07:58 +0200

    Fix code for position in 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -71,5 +71,6 @@ class Square:
             for j in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0], end="")
+                for i in range(self.__position[0]):
+                    print(end=" ")
                 print("#" * self.__size)
