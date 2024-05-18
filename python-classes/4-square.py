b696b56bcf9daf7603d435520d0e986b7d000153
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-18 15:38:29 +0200

    Cretate the code in 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" A class Square that defines a square by:
+    A private instance attribute size
+    and the handling of TypeError and ValueError.
+    A Public instance method that returns the current square area.
+"""
+
+
+class Square:
+    """ Define a Square
+    """
+
+    def __init__(self, size=0):
+        """Initialize a Square.
+
+        Args:
+            size : The size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ To retrieve the private instance attibute size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ To set the private instance attibute size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Define the area of the square
+        """
+        area = self.__size * self.__size
+        return area
