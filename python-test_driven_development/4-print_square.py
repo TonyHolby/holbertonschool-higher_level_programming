0fc58b5d4cfc3ef346a9343adb34f91c416576e5
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-21 22:22:56 +0200

    Create the code in 4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Prints a square with the character #.
+    
+    Parameters:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is not positive.
+"""
+
+
+def print_square(size):
+    """ Prints a square with the character #. """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for index in range(size):
+        print("#" * size)
