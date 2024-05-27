f194da840af2bad8ce3401c2f63081c3f31b90de
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-27 16:36:06 +0200

    Fix code in 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,11 @@ import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 filename = "add_item.json"
-my_obj = []
-my_obj = load_from_json_file(filename)
-my_obj.extend(sys.argv[1:])
-save_to_json_file(my_obj, filename)
+try:
+    item_added = load_from_json_file(filename)
+except (FileNotFoundError, json.JSONDecodeError):
+    item_added = []
+item_added.extend(sys.argv[1:])
+save_to_json_file(item_added, filename)
