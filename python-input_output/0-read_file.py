8c51cdc70974452a9559f514661103dcea2143b5
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-27 09:57:32 +0200

    Write code in 0-readfile.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A function that reads a text file."""
+
+
+def read_file(filename=""):
+    """Open a file, read a text (UTF8) and print it to stdout."""
+    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
+        read_file = f.read()
+    print(read_file, end='')
