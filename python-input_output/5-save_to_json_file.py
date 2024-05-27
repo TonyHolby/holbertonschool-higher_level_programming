c65d6c0a02d54fbeec542e0dbc7a2d5e4bc1acce
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-27 15:11:28 +0200

    Fix code in 5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Import JSON."""
-import json
-
 """ A function that writes an Object to a text file, using a JSON
     representation.
 """
+import json
 
 
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation."""
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'w') as f:
         json.dump(my_obj, f)
