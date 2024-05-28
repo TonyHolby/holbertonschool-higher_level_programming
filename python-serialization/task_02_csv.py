db26608422f725bcf57657d8d698696ad2e0969d
TonyHolby <tonynemoutheholberton@gmail.com>
2024-05-28 19:28:04 +0200

    Fix comments in task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,7 +8,7 @@ import json
 
 
 def convert_csv_to_json(csv_filename):
-    """ Converts data to dictionary list in using serialization techniques.
+    """ Converts data into a dictionary list.
     
     Args:
         csv_filename: The name of the CSV file to convert.
