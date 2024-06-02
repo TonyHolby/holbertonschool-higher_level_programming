bc94f272a3bb735ac64401ccb80d6e733fb42cb3
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-02 19:00:41 +0200

    Fix code and add comments in task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,34 +2,53 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {
-    "jane": {
-        "username": "jane",
-        "name": "Jane",
-        "age": 28,
-        "city": "Los Angeles"
-        },
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-}
+users = {}
+
+""" Defines a route for the root URL. """
 
 
 @app.route('/')
 def home():
+    """ Handles the route.
+
+    Returns: The string: "Welcome to the Flask API!".
+    """
     return "Welcome to the Flask API!"
+
+
+""" Defines a new route and return a JSON response using jsonify(). """
 
 
 @app.route("/data")
 def get_users():
+    """ Stores the users in memory using a dictionary.
+
+    Returns: A list of all the usernames stored in the API.
+    """
     return jsonify(list(users.keys()))
+
+
+""" Defines endpoints to simulate different functionalities. """
 
 
 @app.route("/status")
 def status():
+    """ Returns the status of the request.
+
+    Returns: OK.
+    """
     return "OK"
 
 
 @app.route("/users/<username>")
 def get_user(username):
+    """ Gets a user.
+
+    Args:
+        username: the name of the user.
+
+    Returns: The full object corresponding to the provided username.
+    """
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -37,12 +56,23 @@ def get_user(username):
         return jsonify({"error": "User not found"}), 404
 
 
+""" Defines a new route that accepts POST requests. """
+
+
 @app.route("/add_user", methods=["POST"])
 def add_user():
+    """ Adds the new user to the users dictionary.
+
+    Returns: A confirmation message with the added user's data.
+    """
     data = request.get_json()
     username = data.get("username")
+
+    if not username:
+        return jsonify({"error": "username is required"}), 400
+
     if username in users:
-        return jsonify({"error": "User already exists"}), 400
+        return jsonify({"error": "User already exists"}), 409
 
     users[username] = {
         "username": username,
@@ -53,7 +83,7 @@ def add_user():
     return jsonify({
         "message": "User added",
         "user": users[username]
-    })
+    }), 201
 
 
 if __name__ == "__main__":
