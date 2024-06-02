fab4a61bee480d258f48cf34ce3488e43815c525
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-02 22:46:56 +0200

    test code in task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,23 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 from flask import Flask, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_httpauth import HTTPBasicAuth
-from flask_jwt_extended import JWTManager, create_access_token,\
-    jwt_required, get_jwt_identity
+from flask_jwt_extended import (JWTManager, create_access_token,
+                                jwt_required, get_jwt_identity)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_jwt_secret_key'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
 
 users = {
-    "user1": {"username": "user1",
-              "password": generate_password_hash("password1"),
-              "role": "user"},
-    "admin1": {"username": "admin1",
-               "password": generate_password_hash("password2"),
-               "role": "admin"}
+    "user1": {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+    },
+    "admin1": {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin"
+    }
 }
 
 
@@ -43,10 +48,9 @@ def login():
     user = users.get(username)
     if user and check_password_hash(user['password'], password):
         access_token = create_access_token(identity={
-            "username": username,
-            "role": user['role']})
+            'username': username,
+            'role': user['role']})
         return jsonify(access_token=access_token)
-
     return jsonify({"error": "Invalid credentials"}), 401
 
 
