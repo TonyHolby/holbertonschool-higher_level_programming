1c3c719cab7e7a6f297fd8a2774efaa4654d939d
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-02 19:06:09 +0200

    Fix code in task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -74,16 +74,8 @@ def add_user():
     if username in users:
         return jsonify({"error": "User already exists"}), 409
 
-    users[username] = {
-        "username": username,
-        "name": data.get("name"),
-        "age": data.get("age"),
-        "city": data.get("city")
-    }
-    return jsonify({
-        "message": "User added",
-        "user": users[username]
-    }), 201
+    users[username] = {"username": username, "name": data.get("name"), "age": data.get("age"), "city": data.get("city")}
+    return jsonify({"message": "User added", "user": users[username]}), 201
 
 
 if __name__ == "__main__":
