2e3ab9a6d73c130300e4e4db1679c531cc2a4dfc
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-02 22:49:53 +0200

    Fix code in task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 from flask import Flask, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_httpauth import HTTPBasicAuth
@@ -7,7 +6,7 @@ from flask_jwt_extended import (JWTManager, create_access_token,
                                 jwt_required, get_jwt_identity)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SECRET_KEY'] = 'your_jwt_secret_key'
 auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
