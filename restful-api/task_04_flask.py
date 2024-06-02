2126a36b2ef5d0b9d8dcc8d9b3e422a22d398d7d
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-02 22:12:20 +0200

    Fix code in task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,23 +6,54 @@ app = Flask(__name__)
 users = {}
 
 
-@app.route("/")
+""" Defines a route for the root URL. """
+
+
+@app.route('/')
 def home():
+    """ Handles the route.
+
+    Returns:
+        The string: "Welcome to the Flask API!".
+    """
     return "Welcome to the Flask API!"
+
+
+""" Defines a new route and return a JSON response using jsonify(). """
 
 
 @app.route("/data")
 def data():
+    """ Stores the users in memory using a dictionary.
+
+    Returns:
+        A list of all the usernames stored in the API.
+    """
     return jsonify(list(users.keys()))
+
+
+""" Defines endpoints to simulate different functionalities. """
 
 
 @app.route("/status")
 def status():
+    """ Returns the status of the request.
+
+    Returns:
+        The string: OK.
+    """
     return "OK"
 
 
 @app.route("/users/<username>")
-def user(username):
+def get_user(username):
+    """ Gets a user.
+
+    Args:
+        username (str): the name of the user.
+    Returns:
+        The full object corresponding to the provided username.
+    """
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -30,14 +61,22 @@ def user(username):
         return jsonify({"error": "User not found"}), 404
 
 
+""" Defines a new route that accepts POST requests. """
+
+
 @app.route("/add_user", methods=["POST"])
 def add_user():
+    """ Adds the new user to the users dictionary.
+
+    Returns:
+        A confirmation message with the added user's data or an error message.
+    """
     new_user = request.get_json()
-    username = new_user.get("username")
+    username = data.get("username")
+
     if not username:
         return jsonify({"error": "username is required"}), 400
-    if username in users:
-        return jsonify({"error": "Username already exists"}), 409
+
     users[username] = new_user
     return jsonify({"message": "User added", "user": users[username]}), 201
 
