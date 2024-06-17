ba36cec453db305fe65892fa36b2e0ece0c5ea59
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-17 21:11:57 +0000

    Fix code and indentation in 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,27 @@
--- A script that prints the following description of the table first_table
--- from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+-- A script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+SELECT
+	t.table_name AS "Table",
+	CONCAT(
+		"CREATE TABLE `", t.table_name, "` (\n",
+		GROUP_CONCAT(
+			CONCAT(
+				"  `", c.column_name, "` ",
+				c.column_type, ' DEFAULT ',
+				IF(c.column_default IS NULL,
+					"NULL",
+					CONCAT('\'', c.column_default, '\'')
+				)
+			)
+		ORDER BY c.ordinal_position
+		SEPARATOR ',\n'
+		),
+		'\n) ENGINE=', t.engine,
+		' DEFAULT CHARSET=', SUBSTRING_INDEX(t.table_collation, '_', 1),
+		' COLLATE=', t.table_collation
+	) AS "Create Table"
+FROM information_schema.tables t
+JOIN information_schema.columns c
+ON t.table_name = c.table_name AND t.table_schema = c.table_schema
+WHERE t.table_schema = 'hbtn_0c_0' AND t.table_name = 'first_table'
+GROUP BY t.table_name, t.engine, t.table_collation;
 
