6f81de60a3ff0f2b883a57c3d94dc4161f132607
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-19 21:37:14 +0000

    Fix code in 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
--- A script that creates the database hbtn_0d_1 and the user user_0d_1.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_1;
+-- A script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
-GRANT SELECT ON hbtn_0d_1.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
