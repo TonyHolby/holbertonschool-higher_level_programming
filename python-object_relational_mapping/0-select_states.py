0dffc53ae891b3f4fc89df12a065ea5488682794
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-21 21:01:09 +0200

    Write code in 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+""" A method"""
+
+
+def list_states(mysql_username, mysql_password, database_name):
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+
+list_states(mysql_username, mysql_password, database_name)
