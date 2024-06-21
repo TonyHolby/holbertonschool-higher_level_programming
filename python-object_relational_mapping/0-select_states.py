b2883af9df72feb5654eb42dee4dec96f0c6c328
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-21 22:43:08 +0200

    Fix code in 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
+
+""" A method that lists all states from the database hbtn_0e_0_usa. """
 import sys
 import MySQLdb
-""" A method that lists all states from the database hbtn_0e_0_usa. """
-
-
-def list_states(mysql_username, mysql_password, database_name):
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print("({}, '{}')".format(row[0], row[1]))
-    cur.close()
-    conn.close()
 
 
 if __name__ == "__main__":
@@ -23,4 +13,13 @@ if __name__ == "__main__":
         mysql_username = sys.argv[1]
         mysql_password = sys.argv[2]
         database_name = sys.argv[3]
-        list_states(mysql_username, mysql_password, database_name)
+
+
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
