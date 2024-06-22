dcd22ff7eb25f8097d5193b6d37aba55a257e1bc
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-22 16:22:17 +0200

    Fix code in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,20 +12,23 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name_searched = sys.argv[4]
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=mysql_username,
-                           passwd=mysql_password,
-                           db=database_name,
-                           charset="utf8")
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states \
-            WHERE states.name = '{}' \
-                ORDER BY states.id ASC".format(state_name_searched)
-                )
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    try:
+        db_connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
+        cursor = db.cursor()
+        cursor.execute(
+            "SELECT * FROM states \
+                WHERE states.name = '{}' \
+                    ORDER BY states.id ASC".format(state_name_searched)
+                    )
+        query_rows = cursor.fetchall()
+        for row in query_rows:
+            print(row)
+    finally:
+        cursor.close()
+        db_connection.close()
