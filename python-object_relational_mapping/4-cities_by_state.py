9a8153b3cb6d2ece2e0f9ee16ef64de0c3bd70b9
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-22 17:05:26 +0200

    Fixx comment in 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-""" A method that takes in arguments and displays all values in the
-    states table of hbtn_0e_0_usa where name matches the argument
-    and that is safe from MySQL injections.
-"""
+""" A method that lists all cities from the database hbtn_0e_4_usa. """
 import sys
 import MySQLdb
 
