0593c89b94a14c5e055eb2d354c5a7da78a986f3
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-22 12:03:46 +0200

    Write code in 2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" A method that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=mysql_username,
+                           passwd=mysql_password,
+                           db=database_name,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name='Arizona'\
+                ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
