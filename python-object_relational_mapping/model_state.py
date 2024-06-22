51222f99d866eb7bfd5414958acee2c06dcfaec4
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-22 22:53:21 +0200

    Fix code in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
 """ A class that defines a State and an instance Base = declarative_base(). """
-import sys
-from sqlalchemy import Column, Integer, String, create_engine
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
 
-engine = create_engine(f"""mysql://{mysql_username}:{mysql_password}
-                       @localhost:3306/{database_name}""")
 Base = declarative_base()
 
 
@@ -19,17 +14,11 @@ class State(Base):
      Defines a state in the database.
 
     Attributes:
+        __tablename__ (str): The name of the MySQL table.
         id (int): The id of the state.
         name (str): The name of the state.
-        charset (str): The encodage of the table.
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    __table_args__ = {'mysql_charset': 'latin1'}
-
-
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
