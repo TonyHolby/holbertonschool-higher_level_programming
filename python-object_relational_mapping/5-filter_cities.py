742cd8305394c7c40ee1784610184ea715721fb1
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-24 00:26:49 +0200

    Fix comment in 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" A method that takes in the name of a state as an argument and lists
+""" A script that takes in the name of a state as an argument and lists
     all cities of that state, using the database hbtn_0e_4_usa.
 """
 import sys
