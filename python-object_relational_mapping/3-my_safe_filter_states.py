999f8b1eb58076b82f6151b9d28c58ee941acad0
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-24 00:30:04 +0200

    Fix comment in 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" A method that takes in arguments and displays all values in the
+""" A script that takes in arguments and displays all values in the
     states table of hbtn_0e_0_usa where name matches the argument
     and that is safe from MySQL injections.
 """
