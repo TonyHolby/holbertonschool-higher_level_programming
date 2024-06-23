9657a108b34cbf6666c674826293812ebd989e4f
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-24 00:09:51 +0200

    Fix comments in model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+
+""" A class that defines a city and inherits from Base. """
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
 
 class City(Base):
+    """
+     Defines a city in the database.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table.
+        id (int): The id of the city.
+        name (str): The name of the city.
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
