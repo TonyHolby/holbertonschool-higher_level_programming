b4b074e8f6c6bc23108a69bcecd65e57befec468
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-23 10:42:48 +0200

    Fix pycodestyle in 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
