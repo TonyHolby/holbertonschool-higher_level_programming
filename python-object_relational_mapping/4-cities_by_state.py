1f937fe83b5d4ff52f3763d3f87eabe80b97c384
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-24 00:28:25 +0200

    Fix comment in 4-city_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" A method that lists all cities from the database hbtn_0e_4_usa. """
+""" A script that lists all cities from the database hbtn_0e_4_usa. """
 import sys
 import MySQLdb
 
