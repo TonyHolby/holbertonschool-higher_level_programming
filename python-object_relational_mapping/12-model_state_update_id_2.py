7da4668012c506aa4a2e8cc42318637dc5825191
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-23 23:53:26 +0200

    Fix comment in 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" A class  that changes the name of a State object
+""" A script that changes the name of a State object
     from the database hbtn_0e_6_usa.
 """
 import sys
