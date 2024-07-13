0edea339e3b0661af4357335ac0458c1fc6d27e7
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-13 20:53:42 +0200

    Fix the SQL request in 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                            db=database_name,
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
+    cur.execute("SELECT * FROM states WHERE BINARY states.name LIKE 'N%'\
                 ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
