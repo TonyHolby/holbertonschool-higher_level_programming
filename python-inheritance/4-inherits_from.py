2fdee1c8a045795588b344620fc7669708a096b7
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-14 23:48:04 +0200

    Fix PEP8 validation in 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,6 +14,6 @@ def inherits_from(obj, a_class):
         True: if obj is an instance of a class that inherited.
         False: Otherwise.
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
