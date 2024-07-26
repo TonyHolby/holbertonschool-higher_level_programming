9aa6d8a9999680a3b596b78879cfabab74c672a0
TonyHolby <tonynemoutheholberton@gmail.com>
2024-07-26 15:55:54 +0200

    Fix the add 'item' in 4-script.js

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('#add_item');
     button.addEventListener('click', function() {
         const newelement = document.createElement('li');
-        newelement.textContent = '<li>Item</li>';
+        newelement.textContent = 'Item';
         const myList = document.querySelector('.my_list');
         myList.appendChild(newelement);
     });
